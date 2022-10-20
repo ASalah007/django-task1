823e5ad3556a71488c2f56a58d13fb7d0e17a143
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2022-10-20 14:45:09 +0200

    task1

--- student/views.py
+++ student/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
+from django.http import JsonResponse
+import json
+import os
 
-# Create your views here.
+
+def list(request):
+    path = os.path.join(os.getcwd(), "student/db.json")
+    with open(path) as fp:
+        data = json.load(fp)
+    return JsonResponse(data, safe=False)
+
+def update_delete(req, id):
+    path = os.path.join(os.getcwd(), "student/db.json")
+
+    if req.method == "POST":
+        with open(path, "r+") as fp:
+            data = json.load(fp)
+            for i, obj in enumerate(data):
+                if obj["id"] == id:
+                    del data[i]
+                    fp.truncate(0)
+                    fp.seek(0)
+                    json.dump(data, fp)
+                    return HttpResponse("<h1>Deleted</h1>")
+        return HttpResponse("<h1>not found</h1>")
+
+    elif req.method == "PUT":
+        with open(path, "r+") as fp:
+            data = json.load(fp)
+            new_obj = json.loads(req.body)
+            for i, obj in enumerate(data):
+                if obj["id"] == id:
+                    data[i] = new_obj
+                    fp.truncate(0)
+                    fp.seek(0)
+                    json.dump(data, fp)
+                    return HttpResponse("<h1>Updated</h1>")
+        return HttpResponse("<h1>not found</h1>")
+        
+
+    return HttpResponse("nothing is here")
+
+
