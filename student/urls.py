8bf6590380d1073dd51669874e08ef94d31882ad
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2022-10-21 19:23:11 +0200

    task2

--- student/urls.py
+++ student/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import list, update_delete
+
+from student.views import StudentListView, StudentEditView
 
 urlpatterns = [
-    path('', list),
-    path("<str:id>/", update_delete)
+    path('', StudentListView.as_view()),
+    path("<int:id>/", StudentEditView.as_view())
 ]
--- student/views.py
+++ student/views.py
@@ -1,44 +1,66 @@
-from django.shortcuts import HttpResponse, render
-from django.http import JsonResponse
 import json
-import os
+
+from django.http import Http404, JsonResponse
+from django.shortcuts import HttpResponse, render
+from django.views import View
+from django.core.exceptions import ObjectDoesNotExist
+from django.core import serializers
 
 
-def list(request):
-    path = os.path.join(os.getcwd(), "student/db.json")
-    with open(path) as fp:
-        data = json.load(fp)
-    return JsonResponse(data, safe=False)
+from .models import Student
 
-def update_delete(req, id):
-    path = os.path.join(os.getcwd(), "student/db.json")
+class StudentListView(View):
 
-    if req.method == "POST":
-        with open(path, "r+") as fp:
-            data = json.load(fp)
-            for i, obj in enumerate(data):
-                if obj["id"] == id:
-                    del data[i]
-                    fp.truncate(0)
-                    fp.seek(0)
-                    json.dump(data, fp)
-                    return HttpResponse("<h1>Deleted</h1>")
-        return HttpResponse("<h1>not found</h1>")
-
-    elif req.method == "PUT":
-        with open(path, "r+") as fp:
-            data = json.load(fp)
-            new_obj = json.loads(req.body)
-            for i, obj in enumerate(data):
-                if obj["id"] == id:
-                    data[i] = new_obj
-                    fp.truncate(0)
-                    fp.seek(0)
-                    json.dump(data, fp)
-                    return HttpResponse("<h1>Updated</h1>")
-        return HttpResponse("<h1>not found</h1>")
+    def get(self, req):
+        all_students = Student.objects.all()
+        all_students_json = serializers.serialize("json", all_students)
+        return JsonResponse(json.loads(str(all_students_json)), safe=False)
         
 
-    return HttpResponse("nothing is here")
+class StudentEditView(View):
 
+    def get(self, req, id):
+        try:
+            student = Student.objects.get(id=id)
+        except ObjectDoesNotExist:
+            return HttpResponse("<h1> object doesn't exist </h1>")
+
+        student_json = serializers.serialize("json", [student])
+        return JsonResponse(json.loads(student_json)[0]["fields"], safe=False)
+        
+
+    def delete(self, req, id):
+        try:
+            student = Student.objects.get(id=id)
+            student.delete()
+        except ObjectDoesNotExist:
+            return HttpResponse("<h1> object doesn't exist </h1>")
+
+        return HttpResponse("<h1>Deleted</h1>")
+
+    def put(self, req, id):
+        s = json.loads(req.body.decode('UTF-8'))
+        Student.objects.create(first_name=s["first_name"],
+                               age=int(s["age"]),
+                               class_room=s["class_room"], 
+                               last_name=s["last_name"],
+                               email=s["email"]
+                               )
+        return HttpResponse("new student added")
+
+    def post(self, req, id):
+        try:
+            student = Student.objects.get(id=id)
+        except ObjectDoesNotExist:
+            return HttpResponse("<h1> object doesn't exist </h1>")
+
+        newstudent = json.loads(req.body.decode('UTF-8'))
+        student.first_name = newstudent["first_name"];
+        student.last_name = newstudent["last_name"];
+        student.age = newstudent["age"];
+        student.class_room = newstudent["class_room"];
+        student.email = newstudent["email"];
+
+        student.save()
+        return HttpResponse("<h1>Updated</h1>")
 
